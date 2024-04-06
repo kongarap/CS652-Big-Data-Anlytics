3108445b37c0836ed6a425891ddbd4c07ca3b649
kongarap <166187491+kongarap@users.noreply.github.com>
2024-04-05 21:22:44 -0400

    Add files via upload

---
+++ class 1.R
@@ -0,0 +1,79 @@
+# Printing Hello World
+"Hello, World"
+# Printing Sum Of Operations
+A <- 5
+B <- 10
+C <-A+B
+C
+# Printing Quotient Of Operation
+v1 <- 4.5
+v2 <- 2
+v3 <- v1/v2
+v3
+# Printing More Variables
+greet <- "Hello"
+name <- "Prashanthi"
+message <- paste(greet, name)
+message
+# Printing Type of data
+x <- 2L
+typeof(x)
+y <- 2.5
+typeof(y)
+# Printing data type
+y <- 2.5
+typeof(y)
+# Printing Dtype
+z <- 2+3i
+typeof(z)
+a <- "h"
+typeof(a)
+q <- T
+typeof(q)
+p <- FALSE
+typeof(p)
+# Printing Basic Math
+a <- 5
+b <- 3
+c <- a+b
+d <- a-b
+e <- a*b
+f<- a/b
+a
+b
+c
+d
+e
+f
+# Printing basic logic using sic operations and complex operations
+4 <5
+10>100
+4==5
+result <- 4 < 5
+result
+typeof(result)
+result <- 4 < 5
+result
+typeof(result)
+result2 <- !(5>1)
+result2
+result | result2
+result & result2
+isTrue(result)
+isTRUE(result)
+# Printing average of three numbers
+a <- 7
+b <- 12
+c <- 5
+average <- (a + b + c) / 3
+print(paste("The average of", a, ",", b, ", and", c, "is:", average))
+# Printing “Welcome to R,<your first name>”
+greet <- "Welcome to R"
+name <- "Prashanthi"
+message <- paste(greet, name)
+message
+# Printing out which variable is bigger
+x <- 7
+y <- 12
+z <- 4
+
